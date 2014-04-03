daf9ba4a1e130ae6c09627551ca6ad7e596a6ed6
olen501@gmail.com <olen501@gmail.com@a6406adc-5bd1-07e4-b2d0-a0d243ce3459>
2014-04-03 15:48:26 +0000

    nav system

---
+++  battle-of-endor/navigation_system.py
@@ -0,0 +1,27 @@
+# Navigation system
+
+class NavigationSystem(object):
+	def __init__(self):
+		pass
+
+	def goToLocation(self, Vec3):
+		pass
+
+	def evade(self, attacker):
+		pass
+
+	def pursue(self, target):
+		pass
+
+	def avoidAread(self, Vec3, r):
+		pass
+
+	def removeAvoidArea(self, area):
+		pass
+
+	def checkCollisionCourse(self):
+		pass
+
+	def findNearbyShips(self):
+		pass
+		
