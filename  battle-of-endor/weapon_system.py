d86989be753e5fbd193c392fd57fda471d0a41a7
saneal929@gmail.com <saneal929@gmail.com@a6406adc-5bd1-07e4-b2d0-a0d243ce3459>
2014-04-03 16:00:36 +0000

    weapon system skeleton file

---
+++  battle-of-endor/weapon_system.py
@@ -0,0 +1,14 @@
+from Ship import Ship
+
+class Weapon_System:
+
+	def __init__(self):
+		pass
+	def engageShip(self, Ship s):
+		pass
+	def selectWeapon(self):
+		pass
+	def fireWeapon(self):
+		pass
+	def activateWeapons():
+		pass
