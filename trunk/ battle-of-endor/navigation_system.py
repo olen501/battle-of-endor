4e58ce31fd2e491b4f2f0225bc5d7c9fb7eb3dac
bstack88@gmail.com <bstack88@gmail.com@a6406adc-5bd1-07e4-b2d0-a0d243ce3459>
2014-04-11 20:34:55 +0000

    FlyInCircle adjusts the heading and pitch of the actor. Going to try to make this more general

--- trunk/ battle-of-endor/navigation_system.py
+++ trunk/ battle-of-endor/navigation_system.py
@@ -18,14 +18,15 @@ from direct.actor.Actor import Actor
 # from navigation_system import NavigationSystem
 
 class NavigationSystem(object):
-	def __init__(self, swactor, timestep):
+	def __init__(self, swActor, timestep):
 
-		self.swactor = swactor
+		self.swActor = swActor
 		self.timestep = timestep
 
 		self.position = Vec3()
 		self.velocity = Vec3()
-		self.heading = Vec3()
+		self.heading = Vec3(1,0,0)
+		self.headingGlobal = Vec3(0,1,0)
 
 		# Position control
 		self.errorOld = Vec3()
@@ -33,10 +34,69 @@ class NavigationSystem(object):
 
 		# PID controller for the swactor acceleration
 		self.accControl = PID(0.2, 0.1, 0.1)
-				
+
+		self.i = 0
+
+	def flyInCircle(self):
+		theta = self.i / ((2*pi)) % (4*pi);
+		self.i = self.i + 0.1
+		r = 0.50
+
+		# Determine new velocity
+		x = r*cos(theta)
+		y = -r*cos(theta)
+		z = -r*sin(theta)
+		vel = Vec3(x,y,z)
+		
+		# Normalize the velocity for later
+		velNorm = Vec3(vel)
+		velNorm.normalize()
+
+		# Coordinate axes for global
+		xh = Vec3(1,0,0)
+		yh = Vec3(0,1,0)
+		zh = Vec3(0,0,1)
+
+		# Project the velocity onto the coordinate axes
+		px = vel.project(xh)
+		py = vel.project(yh)
+		pz = vel.project(zh)
+
+		# Determine the change in position...maybe add acceleration?
+		posDelta = Vec3(px.getX(), py.getY(), pz.getZ())#*time + 1/2at^2
+
+		# Store the updated position
+		self.position = self.position + posDelta
+		self.swActor.setPos(self.position)
+
+		# Project velocity onto the xy plane, then calcualte the angle to x
+		hProj = Vec3(vel.getX(), vel.getY(), 0)
+		hProjN = Vec3(hProj)
+		hProjN.normalize()
+
+		# Determine the angle between the projection of the velocity onto the
+		# XY plane and xh
+		h = hProjN.angleDeg(xh)
+		
+		# Panda3D will return the smaller of the two angles. We need complete
+		# rotation, and this calculation gives us the correct heading
+		if(vel.getY() <= 0):
+			h = 360 - h
+
+		# Determine the pitch
+		p = velNorm.angleDeg(zh)
+		
+		# Update the heading and pitch of the actor
+		# The -90 is an artifact of all models not being aligned. This needs to be
+		# resolved!
+		self.swActor.setH(h-90)
+		self.swActor.setP(90-p)
+
+
+
 	def goToLocation(self, loc):
 
-		curLoc = self.swactor.getPos()
+		curLoc = self.swActor.getPos()
 		error = loc - curLoc
 
 		# Run the PID controller for the acceleration
@@ -47,11 +107,14 @@ class NavigationSystem(object):
 		self.heading = self.velocity / self.velocity.length()
 
 		self.position = self.position*self.timestep + accel*self.timestep*self.timestep/2
-		self.swactor.setPos(self.position)
+		self.swActor.setPos(self.position)
 
 		# Store error
 		self.errorOld = error
 
+
+	def updatePosition(self):
+		self.position = self.position + self.velocity*dt
 		
 
 	def evade(self, attacker):
